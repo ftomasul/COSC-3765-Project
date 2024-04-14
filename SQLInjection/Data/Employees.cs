a923aac5cb7ab3df255664d3a9a89871daf6ffa3
Finn Tomasula Martin <ftomasul@uwyo.edu>
2024-04-14 07:52:12 -0600

    Finished base page

--- SQLInjection/Data/Employees.cs
+++ SQLInjection/Data/Employees.cs
@@ -3,10 +3,8 @@
     public class Employee
     {
         public int Id { get; set; }
-        public string? FirstName { get; set; }
-        public string? LastName { get; set; }
-        public string? Email { get; set; }
-        public string? Department { get; set; }
-        public string? Avatar { get; set; }
+        public string? Name {get; set;}
+        public string? Username {get; set;}
+        public string? Password {get; set;}
     }
 }
---
+++ SQLInjection/Migrations/20240413190107_InitialCreate.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace SQLInjection.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Employee",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: true),
+                    Username = table.Column<string>(type: "TEXT", nullable: true),
+                    Password = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employee", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Employee",
+                columns: new[] { "Id", "Name", "Password", "Username" },
+                values: new object[,]
+                {
+                    { 1, "Frank", "pass123", "frank1" },
+                    { 2, "Bob", "safe321", "coolguy" },
+                    { 3, "Sarah", "word17", "haras" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employee");
+        }
+    }
+}
